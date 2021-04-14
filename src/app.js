1669833d50f6e5081377214f602a7686306d215e
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-04-14 10:09:05 -0300

    Setup heroku settings

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000; // Heroku port
 
 // Define paths for exporess config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -94,6 +95,6 @@ app.get('*', (req, res) => {
 
 
 // Listen Server
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Serve up on port 3000.');
 });
